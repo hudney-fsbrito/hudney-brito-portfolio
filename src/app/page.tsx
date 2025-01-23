4d641226427f57246cef01533a318e51c1b7dd9f
hudney-fsbrito <hudney.rse@gmail.com>
2025-01-23 11:09:11 -0300

    style: cria variáveis css e inicia página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,11 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-       
-
+        <h1>Dev. Web</h1>
+        <button>x</button>
         
       </main>
       <footer className={styles.footer}>
-        
       </footer>
     </div>
   );
